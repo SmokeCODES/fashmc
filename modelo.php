35fb4a0272c40ae0cb9945de6211883fa7df6ede
Samuel P. Silva <samuelpvp20@gmail.com>
2018-12-22 13:54:29 -0200

    Commit 1°

---
+++ modelo.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loja | FashMC</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/loja.css">
+</head>
+<body>
+	<img src="../assets/images/logo.png" class="image">
+	<div class="topo">
+		<div class="menu">
+			<ul>
+				<a href="../" class="cor">
+					<li>
+						Inicio
+					</li>
+				</a>
+				<a href="../loja/" class="cor">
+					<li>
+						Loja
+					</li>
+				</a>
+				<a href="../forum/" class="cor">
+					<li>
+						Forum
+					</li>
+				</a>
+				<a href="../perfil/" class="cor">
+					<li>
+						Perfil
+					</li>
+				</a>
+			</ul>
+
+			<ul class="login">
+				<a href="../login/" class="cor">
+					<li>
+						Login
+					</li>
+				</a>
+				<a href="../cadastro/" class="cor">
+					<li>
+						Cadastro
+					</li>
+				</a>
+			</ul>
+		</div>
+	</div>
+	<a href="">
+		<img src="../assets/images/teste2.png" class="imageface"><br/>
+	</a>
+	<a href="">
+		<img src="../assets/images/twitter.jpg" class="twitter">
+	</a>
+</body>
+</html>
